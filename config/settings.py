2a64b2180849886b8cb6d7378535d0cabdd71106
WPH95 <wph95@Peter-WangdeMacBook-Pro.local>
2015-09-01 17:19:29 +0800

    V1.7.3

--- config/settings.py
+++ config/settings.py
@@ -91,7 +91,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 #         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 #     }
 # }
-DAO_TEST = bool(os.environ['MYSQL_USERNAME'])
+DAO_TEST = bool(os.environ['DAO_TEST'])
 if DAO_TEST:
     DATABASES = {
         'default': {
